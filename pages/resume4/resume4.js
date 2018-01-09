394ef6a01525ff2501c57001879d7c0880fed2b6
Recklesslmz <936838918@qq.com>
2018-01-09 16:22:39 +0800

    first commit

---
+++ pages/resume4/resume4.js
@@ -0,0 +1,24 @@
+Page({
+    data: {
+        animationData: {},
+        checkOpacity: 0
+    },
+
+
+    onShow: function() {
+        var animation = wx.createAnimation({
+            duration: 900,
+            timingFunction: 'linear',
+        })
+        this.animation = animation
+        animation.opacity(1).translateY(10).step()
+        this.setData({
+            animationData: animation.export(),
+        })
+    },
+    toNext() {
+        wx.navigateTo({
+            url: '../resume5/resume5'
+        })
+    }
+})
